aa41955b90e11295cdfc78ba7537811fd53a9ce1
lathanh1108 <lathanh1108@gmail.com>
2016-10-22 10:24:30 +0700

    còn thiêu jquery

---
+++ gachMenLongKhanh/gachMenLongKhanh/Scripts/Front/loaisanpham.js
@@ -0,0 +1,32 @@
+﻿/*Nút chuyển ở danh mục ngang banner*/
+$(function () {
+
+    /*Chuyển xuống danh sách sản phẩm*/
+    $("#next-to-danhsachsanpham").click(function () {
+        $('html, body').animate({
+            scrollTop: $('#danhsachsanpham').offset().top
+        }, 500);
+    });
+
+    /*Chuyển xuống bộ sưu tập ảnh*/
+    $("#next-to-bosuutap").click(function () {
+        $('html, body').animate({
+            scrollTop: $('#bosuutap').offset().top
+        }, 500);
+    });
+
+    /*Chuyển xuống bản đồ*/
+    $("#next-to-bando").click(function () {
+        $('html, body').animate({
+            scrollTop: $('#bando').offset().top
+        }, 500);
+    });
+
+    /*Chuyển xuống liên hệ*/
+    $("#next-to-lienhe").click(function () {
+        $('html, body').animate({
+            scrollTop: $('#lienhe').offset().top
+        }, 500);
+    });
+});
+
